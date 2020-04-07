5fa4778b6fea545f50df291b7df8456f088e39ad
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-07 15:04:16 -0400

    added function to build a data object for useage in the readme template. created new files modularization.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
+const fs = require('fs');
 // questions 
 const questions = [
   {
@@ -27,45 +28,47 @@ const questions = [
     message: "Enter License info",
     name: "license"
   },
-  {
-    type: "input",
-    message: "Enter Project Title",
-    name: "title"
-  },
 ]
-const userAnswer = [];
-getInfo()
-//asks user for github username. searches github with input
-function getInfo() {
-  inquirer
-    .prompt({
-      message: "Enter your GitHub username",
-      name: "username"
-    })
-    .then(function ({ username }) {
-      const queryUrl = `https://api.github.com/users/${username}`;
-      axios.get(queryUrl)
-        .then(function (res) {
-          console.log(res.data.avatar_url);
-          console.log(res.data.email);
 
-          promptUser()
-        });
-    });
-}
+
+const userAnswer = {};
+// getInfo()
+// //asks user for github username. searches github with input
+// function getInfo() {
+//   inquirer
+//     .prompt({
+//       message: "Enter your GitHub username",
+//       name: "username"
+//     })
+//     .then(function ({ username }) {
+//       const queryUrl = `https://api.github.com/users/${username}`;
+//       axios.get(queryUrl)
+//         .then(function (res) {
+//           console.log(res.data.avatar_url);
+//           console.log(res.data.email);
+
+//           promptUser()
+//         });
+//     });
+// }
+promptUser()
 async function promptUser() {
   for (let i = 0; i < questions.length; i++) {
     await inquirer.prompt(questions[i])
       .then(function (answer) {
-          userAnswer.push(answer);
+       let key = Object.keys(answer)[0]
+       console.log(answer)
+       console.log(key)
+          userAnswer[key] = answer[key];
         })
   }
-  buildReadme()
+ console.log(userAnswer) 
+ 
 }
-function buildReadme(){
-  userAnswer.forEach(x => {
-   let key = Object.keys(answer)[0];
-   if(userAnswer[x].key){}
-  });
-}
+// function buildReadme(){
+//   userAnswer.forEach(x => {
+//    let key = Object.keys(answer)[0];
+//    if(userAnswer[x].key){}
+//   });
+// }
 
