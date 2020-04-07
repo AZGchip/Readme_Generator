9530360eb3b50335b9c094bddc19bf3bba41cb89
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-06 20:05:30 -0400

    added function to prompt the user with questions from an array

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
+// questions 
+const questions = [
+  {
+    type: "input",
+    message: "Enter Project Title",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Enter Project Description",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Enter Project Installation Info",
+    name: "install"
+  },
+  {
+    type: "input",
+    message: "Enter Usage Information",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Enter License info",
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "Enter Project Title",
+    name: "title"
+  },
+]
+const userAnswer = [];
 getInfo()
-function getInfo(){
-inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
-  .then(function ({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
-    axios.get(queryUrl)
-      .then(function (res) {
-        console.log(res.data.avatar_url);
-        console.log(res.data.email);
-      });
+//asks user for github username. searches github with input
+function getInfo() {
+  inquirer
+    .prompt({
+      message: "Enter your GitHub username",
+      name: "username"
+    })
+    .then(function ({ username }) {
+      const queryUrl = `https://api.github.com/users/${username}`;
+      axios.get(queryUrl)
+        .then(function (res) {
+          console.log(res.data.avatar_url);
+          console.log(res.data.email);
+
+          promptUser()
+        });
+    });
+}
+async function promptUser() {
+  for (let i = 0; i < questions.length; i++) {
+    await inquirer.prompt(questions[i])
+      .then(function (answer) {
+          userAnswer.push(answer);
+        })
+  }
+  buildReadme()
+}
+function buildReadme(){
+  userAnswer.forEach(x => {
+   let key = Object.keys(answer)[0];
+   if(userAnswer[x].key){}
   });
 }
+
